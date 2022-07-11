da7a6af5b51479f9057c83d263e048fd0018b0a0
Ayushguptaa22 <Ayushgupta_19021785.cse@geu.ac.in>
2022-07-11 13:17:59 +0530

    data file stored in resources

---
+++ ipldashboard/src/test/java/io/bytethatt/ipldashboard/IpldashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bytethatt.ipldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpldashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
