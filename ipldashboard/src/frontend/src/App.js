5596f3eb6304993016e2e982022f7ea1a8fc112f
Ayushguptaa22 <Ayushgupta_19021785.cse@geu.ac.in>
2022-07-14 03:00:54 +0530

    Router set

--- ipldashboard/src/frontend/src/App.js
+++ ipldashboard/src/frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 
 function App() {
   return (
     <div className="App">
-      <TeamPage />
+      <BrowserRouter>
+        <Route path="/teams/:teamName">
+          <TeamPage />
+          </Route>
+      </BrowserRouter>
     </div>
   );
 }
