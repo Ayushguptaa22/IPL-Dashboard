4ae2139a73f01b25e73838a3a829597d7a5613e3
Ayushguptaa22 <Ayushgupta_19021785.cse@geu.ac.in>
2022-07-13 22:37:56 +0530

    ui interface layout

---
+++ ipldashboard/src/frontend/src/components/MatchDetailCard.js
@@ -0,0 +1,10 @@
+import {React} from 'react';
+
+export const MatchDetailCard = ()=>{
+  return (
+    <div className="MatchDetailCard">
+      <h2>Latest Matches</h2>
+      <h4>Match Details</h4>
+    </div>
+  );
+}
---
+++ ipldashboard/src/frontend/src/components/MatchSmallCard.js
@@ -0,0 +1,9 @@
+import {React} from 'react';
+
+export const MatchSmallCard = ()=>{
+  return (
+    <div className="MatchSmallCard">
+      <p>Match small card</p>
+    </div>
+  );
+}
